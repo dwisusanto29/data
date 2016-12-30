d0f6fd43953419dd91c6b5e94b99c01bb8c9d563
dw.susanto@gmail.com <dw.susanto@gmail.com>
2016-12-30 13:57:35 +0700

    data_20161229.csv

--- clean.py
+++ clean.py
@@ -1,3 +1,3 @@
 from more_itertools import unique_everseen
-with open('data_2016-12.csv','r') as f, open('dataclean-2016-12.csv','w') as out_file:
+with open('data_20161227.csv','r') as f, open('dataclean-20161227.csv','w') as out_file:
     out_file.writelines(unique_everseen(f))
